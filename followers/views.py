4cb23ee882d788fd801f3cbd8751678e37bac2c6
Alma Broman <alma.broman@gmail.com>
2024-03-12 16:43:52 +0000

    fix typo in follower/views

--- followers/views.py
+++ followers/views.py
@@ -13,7 +13,7 @@ class FollowerList(generics.ListCreateAPIView):
         serializer.save(owner=self.request.user)
 
 
-class LikeDetail(generics.RetrieveDestroyAPIView):
+class FollowerDetail(generics.RetrieveDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = FollowerSerializer
     queryset = Follower.objects.all()
