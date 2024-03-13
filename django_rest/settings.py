0a64fe87874567a78f6e58a318179e6e8162bc7f
Alma Broman <alma.broman@gmail.com>
2024-03-13 14:27:46 +0000

    Fix bug allowed hosts var settings

--- django_rest/settings.py
+++ django_rest/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-almabroman-djangorest-hy8knb2uvax.ws-eu110.gitpod.io','django-react-test.herokuapp.com']
+ALLOWED_HOSTS = ['8000-almabroman-djangorest-hy8knb2uvax.ws-eu110.gitpod.io','django-react-test-e8f863c4eb7b.herokuapp.com']
 
 
 # Application definition
